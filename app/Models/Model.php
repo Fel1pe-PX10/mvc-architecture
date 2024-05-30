c29ca0bfa73a3fd28784c4b18ba5e82e0ff05fbb
Felipe Paez <ofpaez87@gmail.com>
2024-05-30 20:13:39 +0200

    Serching: Multiple where in the same query

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -115,9 +115,15 @@ class Model {
             $operator = "=";
         }
 
-        $this->sql = "SELECT SQL_CALC_FOUND_ROWS * FROM {$this->table} WHERE {$column} {$operator} ?";
-        $this->data[] = $value;
-
+        if(empty($this->sql)){
+            $this->sql = "SELECT SQL_CALC_FOUND_ROWS * FROM {$this->table} WHERE {$column} {$operator} ?";
+            $this->data[] = $value;
+        }
+        else{
+            $this->sql .= " AND {$column} {$operator} ?";
+            $this->data[] = $value;
+        }
+        
         return $this;
     }
 
